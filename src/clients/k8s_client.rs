155fd93fd4d90bfa56eaeab8f0a2afbd89558400
André Claudino <claudino@d2x.com.br>
2024-05-21 12:19:05 -0300

    Adiciona suporte a criar ConfigMaps e Secrets

--- src/clients/k8s_client.rs
+++ src/clients/k8s_client.rs
@@ -1,6 +1,5 @@
-use k8s_openapi::api::batch::v1::Job;
+use k8s_openapi::api::{batch::v1::Job, core::v1::{ConfigMap, Secret}};
 use kube::{api::PostParams, Api};
-
 use crate::entities::job::MaestroJob;
 
 pub struct MaestroK8sClient {
@@ -23,5 +22,19 @@ impl MaestroK8sClient {
         let maestro_job = MaestroJob::new(&created_job, self.client.clone());
 
         Ok(maestro_job)
+    }
+
+    pub async fn create_configmap(&self, config_map: ConfigMap, namespace: &str, dry_run: bool) -> anyhow::Result<ConfigMap> {
+        let api = Api::<ConfigMap>::namespaced(self.client.clone(), namespace);
+        let post_parameters = PostParams{ dry_run, ..PostParams::default()};
+        let created = api.create(&post_parameters, &config_map).await?;
+        Ok(created)
+    }
+
+    pub async fn create_secret(&self, secret: Secret, namespace: &str, dry_run: bool) -> anyhow::Result<Secret> {
+        let api = Api::<Secret>::namespaced(self.client.clone(), namespace);
+        let post_parameters = PostParams{ dry_run, ..PostParams::default()};
+        let created = api.create(&post_parameters, &secret).await?;
+        Ok(created)
     }
 }
